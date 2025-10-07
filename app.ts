1e1ee477c70c95c0b89cde1a5480b014cfbfb2b3
66011212243 <66011212243@msu.ac.th>
2025-10-07 07:09:20 +0700

    commit

--- app.ts
+++ app.ts
@@ -41,7 +41,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: '*',
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     // credentials: true
