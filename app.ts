d69604e5756f87f2caf65ad5ddcb3630650ec373
66011212243 <66011212243@msu.ac.th>
2025-10-08 14:31:35 +0700

    commit

--- app.ts
+++ app.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://gamery-web.firebaseapp.com',
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
